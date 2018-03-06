39948a5a1728c6aefd92e4f1c0a88e86ab85070d
at15 <at15@outlook.com>
2018-03-06 11:52:43 -0800

    [test] Fix broken mem node store due to proto

--- pkg/central/store/meta/mem/node_test.go
+++ pkg/central/store/meta/mem/node_test.go
@@ -12,14 +12,14 @@ func TestMetaStore_AddNode(t *testing.T) {
 	assert := asst.New(t)
 
 	m := NewMetaStore()
-	m.AddNode("1", pb.Node{Host: "n1"})
+	m.AddNode("1", pb.Node{Info: pb.NodeInfo{Host: "n1"},})
 	n, err := m.NumNodes()
 	assert.Nil(err)
 	assert.Equal(1, n)
 
 	node, err := m.FindNodeById("1")
 	assert.Nil(err)
-	assert.Equal("n1", node.Host)
+	assert.Equal("n1", node.Info.Host)
 
 	assert.NotNil(m.RemoveNode("2"))
 	assert.Nil(m.RemoveNode("1"))
