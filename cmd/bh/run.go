c2ac9fa742a7c7dd6f38ce967a84410b5efc8edd
at15 <at15@outlook.com>
2020-01-22 17:16:12 -0800

    [client] Add actual run logic

--- cmd/bh/run.go
+++ cmd/bh/run.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"io/ioutil"
-
-	"gopkg.in/yaml.v2"
+	"os"
+	"os/exec"
 
 	"github.com/benchhub/benchhub/bhpb"
+	"github.com/benchhub/benchhub/pkg/gobench"
 	"github.com/dyweb/gommon/errors"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 // run.go allocate a job id, run the benchmark and report result
@@ -24,13 +28,43 @@ var runCmd = &cobra.Command{
 		if err := LoadYAMLTo(DefaultConfig, &spec); err != nil {
 			return err
 		}
+
+		// Register
 		regRes, err := client.RegisterGoBenchmark(context.Background(), &spec)
 		if err != nil {
 			return errors.Wrap(err, "failed to register")
 		}
 		log.Infof("registered job id %d spec id %d", regRes.JobId, regRes.SpecId)
+
+		// Run with redirect
+		if err := runGobench(spec.Command); err != nil {
+			return errors.Wrap(err, "error run benchmark")
+		}
+
+		// Report
+		result, err := gobench.ParseFile(spec.Report.Input)
+		if err != nil {
+			return errors.Wrap(err, "error parse benchmark output")
+		}
+		log.Infof("found %d results", len(result))
+
+		// TODO: Add server side methods for report
 		return nil
 	},
+}
+
+func runGobench(command *bhpb.GoBenchmarkCommandSpec) error {
+	cmd := exec.Command("sh", "-c", command.Command)
+	var buf bytes.Buffer
+	cmdout := io.MultiWriter(&buf, os.Stdout)
+	cmd.Stdout = cmdout
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(command.Output, buf.Bytes(), 0664); err != nil {
+		return err
+	}
+	return nil
 }
 
 func LoadYAMLTo(f string, v interface{}) error {
