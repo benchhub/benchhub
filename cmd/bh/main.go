c65eba8a0620fe2174dbaa2430c6086f372f552b
Pinglei Guo <at15@outlook.com>
2020-06-29 13:52:39 +0800

    [cmd] Use dyweb/dcli for bh cli

---
+++ cmd/bh/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"github.com/dyweb/gommon/dcli"
+	dlog "github.com/dyweb/gommon/log"
+)
+
+var logReg = dlog.NewRegistry()
+var log = logReg.NewLogger()
+
+func main() {
+	root := &dcli.Cmd{
+		Name: "bh",
+		Run: func(ctx context.Context) error {
+			log.Info("bh does nothing")
+			return nil
+		},
+	}
+	dcli.RunApplication(root)
+}
--- core/framework/pkg.go
+++ core/framework/pkg.go
@@ -2,4 +2,3 @@
 // Each framework implementation maps to a existing benchmark framework like TPC-C, go's builtin micro benchmark etc.
 // Implementations can be found in benchhub/frameworks.
 package framework
-
